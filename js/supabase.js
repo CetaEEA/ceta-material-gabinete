71b69017cd38f10c58e54ac1de9450b7172ee82f
CetaEEA <64902696+CetaEEA@users.noreply.github.com>
2026-09-13 17:54:48 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,27 @@
+// =========================================================
+// SUPABASE
+// MATERIAL DE GABINETE CETA
+// =========================================================
 
+
+// =========================================================
+// CONFIGURACIÓN
+// =========================================================
+
+const SUPABASE_URL =
+    "AQUI_TU_SUPABASE_URL";
+
+
+const SUPABASE_ANON_KEY =
+    "AQUI_TU_SUPABASE_ANON_KEY";
+
+
+// =========================================================
+// CLIENTE
+// =========================================================
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
